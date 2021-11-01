8f02f62c6b4fbe90b6efbaf6ca94e0257c218ea7
Jonathan Marcon <jonathanmarcom4@gmail.com>
2021-11-01 15:51:44 -0300

    Menu responsivo

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Mundo Linux</title>
+
+     <!-- Information -->
+     <meta content="Jonathan Marcon" name="author">
+     <meta content="Index, Follow" name="keywords">
+     <meta content="Aprendizado de sistemas Linux" name="description">
+ 
+    <!-- Viewport -->
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!-- Icons -->
+     <link href="recursos/img/icon/icon.jpg" rel="icon">
+     <link href="recursos/img/icon/icon.jpg" rel="apple-touch-icon">
+
+     <!-- Css -->
+     <link rel="stylesheet" href="recursos/css/style.css">
+
+     <!-- Fonts -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+
+
+
+</head>
+<body>
+        <?php
+                include("navbar.php")
+        ?>
+        
+        <header class="cabecalho"> 
+                 <h1> Aqui você vai aprender tudo sobre os SO Linux!</h1> 
+        </header>  
+
+        <footer> </footer>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav>
+        <a class="logo" href="/">Mundo Linux</a>
+        <div class="mobile-menu">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+        <ul class="nav-list">
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Conteudo</a></li>
+          <li><a href="#">Contato</a></li>
+          <li><a href="#">Login</a></li>
+        </ul>
+      </nav>
+    <script src="mobile-navbar.js"></script>
+  
